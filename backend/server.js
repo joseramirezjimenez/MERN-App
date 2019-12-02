3f7d3b7bacd3427efb2779b97df7289056c6a6ff
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-12-02 12:15:28 -0600

    Added the ability to get data and add data to the database from the server, and set up the fronted to carry out those operations

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 //Mongoose - allows us to set up schemas for the data we store in our MongoDB data. We will also set up our database connection using Mongoose.
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 //Express - framework for NodeJS. Handles our routing and connection info (port, ect.).
-const express = require ('express');
+const express = require('express');
 //Morgan - Handles logging stuff for database.
-const logger = require ('morgan');
+const logger = require('morgan');
 //Body-Parser - Makes sure the body of our requests is formatted correctly (in this case, we'll use JSON).
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 
 //Import the getSecret function from the secret.js file
 const getSecret = require('./secret');
+
+//Import the Mongoose Schema for our data
+const Data = require('./DataSchema');
 
 //Constant to hold the port that we are going to use to connect
 const API_PORT = 3001;
@@ -36,6 +39,49 @@ app.use(logger('dev'))
 //Default route that will run whenever we first connect to the server
 router.get('/', (req, res) => {
     res.json({ message: 'HELLO WORLD' });
+});
+
+//Route to retriave data from the database
+router.get('/getData', (req,res) => {
+    //Use Mongoose to find data with the given schema
+    Data.find((err, data) => {
+        console.log(data);
+
+        if(err) {
+            return res.json({ success: false, error: err });
+        }
+
+        return res.json({ success: true, data: data });
+    });
+});
+
+//Route that will put new information in the database
+router.post('/postData', (req, res) => {
+    //A new instance of the Mongoose Schema which we will send to the database
+    let newData = new Data();
+
+    //Pull the ID and message from the body of the request
+    const { id, message } = req.body;
+
+    //If ID doesn't have a value and is not equal to 0 or message doesn't have a value, return an error
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: 'INVALID INPUT'
+        })
+    }
+
+    //Configure the schema object
+    newData.id = id;
+    newData.message = message;
+
+    newData.save(err => {
+        if (err) {
+            return res.json({ success: false, error: err });
+        }
+
+        return res.json({ success: true });
+    });
 });
 
 //Tell Express to use a certain path and to use the router we set up
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      message: null,
+      intervalIsSet: false,
+      idToDelete: null,
+      idToUpdate: null,
+      newMessage: null
+    };
+  }
+
+  componentDidMount() {
+    this.getDataFromDB();
+    //Set up an interval to call the getDataFromDB function every second
+    setInterval(this.getDataFromDB, 1000);
+  }
+
+  getDataFromDB = () => {
+    axios({
+      url: 'http://localhost:3000/api/getData',
+      method: 'GET'
+    }).then((response) => {
+      console.log(response);
+      //Updated our state with the data from the backend
+      this.setState({ data: response.data.data });
+    }).catch((error) => {
+      console.log(error);
+    })
+  };
+  
+
+  postDataToDB = message => {
+    //1. Figure out what ID this message needs to have
+    //2. Use Axios to connect to our API server, which will send the data on to our database
+
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      idToBeAdded++;
+    }
+
+    axios({
+      url: 'http://localhost:3000/api/postData',
+      method: 'POST',
+      data: {
+        id: idToBeAdded,
+        message: message
+      }
+    }).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    })
+  };
+
+  renderListItems() {
+    //Destructing the data object from our state object
+    const { data } = this.state;
+
+    if (data.length === 0) {
+      //If our data array doesn't have any contents, display a message letting the user know that the database doesn't have anything inside it yet.
+      return "NO DB ENTRIES YET";
+    } else {
+      return data.map(dat => (
+        <li key={dat}>
+          <span> id: {dat.id} </span>
+          <span> message: {dat.message} </span>
+        </li>
+      ));
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Display the data we retrieve from the database */}
+        <ul>
+          {this.renderListItems()}
+        </ul>
+
+        <div>
+          <input
+            type='text'
+            placeholder='Add a New Message to the Database'
+            onChange={event => this.setState({ message: event.target.value })}
+          />
+          <button onClick={() => this.postDataToDB(this.state.message)}>ADD</button>
+        </div>
+
+        <div>
+          <input />
+          <button>DELETE</button>
+        </div>
+
+        <div>
+          <input />
+          <input />
+          <button>UPDATE</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
